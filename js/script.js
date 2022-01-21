0115f7435ba1cf999563f543a603b847bfb0bdb1
Arjan Hoek <arjanhoek5191@gmail.com>
2022-01-21 10:36:49 +0100

    implement smooth scrolling

--- js/script.js
+++ js/script.js
@@ -8,3 +8,44 @@ const menuEl = document.querySelector('.btn-mobile-nav');
 menuEl.addEventListener('click', function () {
   menuEl.parentElement.classList.toggle('nav-open');
 });
+
+// Scrolling
+const allLinks = document.querySelectorAll('a:link');
+
+Array.from(allLinks).forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute('href');
+    menuEl.parentElement.classList.remove('nav-open');
+
+    if (href === '#') {
+      // Scroll back to top
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+// Fixing flexbox gap property missing in some Safari versions
+// function checkFlexGap() {
+//   var flex = document.createElement('div');
+//   flex.style.display = 'flex';
+//   flex.style.flexDirection = 'column';
+//   flex.style.rowGap = '1px';
+
+//   flex.appendChild(document.createElement('div'));
+//   flex.appendChild(document.createElement('div'));
+
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
+
+//   if (!isSupported) document.body.classList.add('no-flexbox-gap');
+// }
+// checkFlexGap();
